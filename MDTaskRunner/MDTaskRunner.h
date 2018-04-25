ea771351a09283887b4e54f275502309aec141e3
Michael Leber <michael.leber@markit.com>
2018-04-25 16:42:21 -0600

    Initial Commit

---
+++ MDTaskRunner/MDTaskRunner.h
@@ -0,0 +1,19 @@
+//
+//  MDTaskRunner.h
+//  MDTaskRunner
+//
+//  Created by Michael Leber on 4/25/18.
+//  Copyright © 2018 IHS Markit Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDTaskRunner.
+FOUNDATION_EXPORT double MDTaskRunnerVersionNumber;
+
+//! Project version string for MDTaskRunner.
+FOUNDATION_EXPORT const unsigned char MDTaskRunnerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDTaskRunner/PublicHeader.h>
+
+
